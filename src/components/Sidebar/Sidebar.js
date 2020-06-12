6c2c26d137766385039d1c89f616c49866217493
Besma <db_rabia_cherif@edu.esi.dz>
2020-06-12 22:16:20 +0100

    sidebar added

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,32 @@
+import React from 'react';
+import logo from "assets/icons/logo.svg";
 import './Sidebar.scss'
 
-const Sidebar=()=>{
- return(
-   <div>Sidebar</div>
+const Sidebar=({className, items})=>{
+  return(
+    <div className={`Sidebar relh100vh ${className || ''}`}>
+      {/*<div className="sidebar-background" />*/}
+      <div className='sidebar-wrapper'>
+        <div className="sidebar-logo flex row aife jcc padv20 padh10">
+          <img className='w34 h34' src={logo} alt="logo_image" />
+          <div className='cwhite fs18 marl5'>Faci Hospitality Master</div>
+        </div>
+        <div className="sidebar-items padv20">
+          {items.map((item,i)=>(
+            <div key={item} className='sidebar-item pointer cwhite mar15 pad15 brad5' active={i==0 ?'true':'false'}>{item}</div>
+          ))}
+        </div>
+      </div>
+    </div>
  )
 }
 
+Sidebar.defaultProps={
+  items:[
+    'Accueil',
+    'Clients',
+    'Commandes',
+  ],
+  className:'w270'
+}
 export default Sidebar
--- src/components/main/Home/Home.js
+++ src/components/main/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import './Home.css';
 import foodImage from 'assets/img/food.jpg' ;
+import Sidebar from 'components/Sidebar/Sidebar';
 function Home() {
   return (
-    <div className="Home">
-      <header className="Home-header">
-        <img className="Home-img" src={foodImage} alt='food'/>
+    <div className="flex fdr">
+      <Sidebar />
+      <header className="flex f1 col jcc aic relh100vh blight">
+        <img className="w350 brad10" src={foodImage} alt='food'/>
         <span
-          className="Home-message mart50"
+          className="fs30 mart50 cpurple"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
