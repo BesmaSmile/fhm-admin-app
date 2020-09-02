91abd2b317b3df1368e3ced187daf3d04eff96d9
Besma <db_rabia_cherif@edu.esi.dz>
2020-09-02 08:38:08 +0100

    form updated

--- src/components/misc/Form/Form.js
+++ src/components/misc/Form/Form.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useForm, useWatch, Controller  } from "react-hook-form";
-import { OutlinedInput, InputLabel, FormControl, Select, MenuItem } from '@material-ui/core';
+import { useForm, useWatch, Controller } from "react-hook-form";
+import { OutlinedInput, InputLabel, FormControl, FormControlLabel, Select, MenuItem, Switch } from '@material-ui/core';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogTitle from '@material-ui/core/DialogTitle';
@@ -8,19 +8,19 @@ import { Button } from '@material-ui/core';
 import _ from 'lodash';
 import './Form.scss';
 
-const FormLayout=({isDialog,title, content, submitAction, submitButton, cancelDisabled, cancel})=>{
-  if(isDialog) return (
+const FormLayout = ({ isDialog, title, content, submitAction, submitButton, cancelDisabled, cancel }) => {
+  if (isDialog) return (
     <div className='Form'>
-       <DialogTitle>{title}</DialogTitle>
+      <DialogTitle>{title}</DialogTitle>
       <DialogContent>
-      <div className='form-content'>{content}</div>
+        <div className='form-content'>{content}</div>
       </DialogContent>
       <DialogActions>
-      <Button variant="contained"
+        <Button variant="contained"
           disabled={cancelDisabled}
           classes={{
             root: 'frm-cancelButton'
-          }} 
+          }}
           onClick={cancel}>Annuler
           </Button>
         {submitButton(submitAction)}
@@ -28,7 +28,7 @@ const FormLayout=({isDialog,title, content, submitAction, submitButton, cancelDi
     </div>
 
   )
-  else return(
+  else return (
     <form className='Form' onSubmit={submitAction} >
       {content}
       <div className='frm-largeButton relw100 mart20'>{submitButton()}</div>
@@ -36,128 +36,141 @@ const FormLayout=({isDialog,title, content, submitAction, submitButton, cancelDi
   )
 }
 
-const Form=(props)=>{
-  const {title, inputs, onSubmit, submitText, pending, disabled, isDialog, cancel}=props
-  const { handleSubmit, errors, getValues , control, setValue } = useForm();
-
-  const defaultValues={}
-  inputs.forEach(input=>{
-    if(input.defaultValue) defaultValues[input.name]=input.defaultValue
+const Form = (props) => {
+  const { title, inputs, onSubmit, submitText, pending, disabled, isDialog, cancel } = props
+  const { handleSubmit, errors, getValues, control, setValue } = useForm();
+  console.log(getValues())
+  const defaultValues = {}
+  inputs.forEach(input => {
+    if (input.defaultValue) defaultValues[input.name] = input.defaultValue
   })
-  
-  const dependentSelection =useWatch({
+
+  const dependentSelection = useWatch({
     control,
     defaultValue: defaultValues
   })
-  
-  const resetWatchingInputs=(e)=>{
-    const {name}=e.target
-    const watchingInputs=inputs.filter(input=>input.watch===name)
+
+  const resetWatchingInputs = (e) => {
+    const { name } = e.target
+    const watchingInputs = inputs.filter(input => input.watch === name)
     watchingInputs.forEach(watchingInput => {
       setValue(watchingInput.name)
     });
   }
 
-  const cleanAndSubmit=(values)=>{
-    const toSend=_.omitBy(values,_.isEmpty);
+  const cleanAndSubmit = (values) => {
+    console.log(values)
+    const toSend = _.omitBy(values, value=>_.isNil(value) || value==='');
+    console.log(toSend)
     onSubmit(toSend)
   }
 
-  const formContent=()=>(    
-      <div className='flex col'>
+  const formContent = () => (
+    <div className='flex col'>
       {
-        inputs.map(input=>{
-          if(input.content)
+        inputs.map(input => {
+          if (input.content)
             return input.content
-          else{
-            const additionalValidation = input.combinedValdation 
-            ? { validate : value=> input.combinedValdation(getValues()) } : {}
+          else {
+            const type =['select', 'switch'].find(t=>t===input.type) || 'default'
+            const additionalValidation = input.combinedValdation ? { validate: value => input.combinedValdation(getValues()) } : {}
             const adornments = {
-              endAdornment : input.endAdornment,
-              startAdornment : input.startAdornment
+              endAdornment: input.endAdornment,
+              startAdornment: input.startAdornment
             }
-            
-            return(
+
+            return (
               <>
-              {(input.type!=='select' || input.options(dependentSelection[input.watch]))
-              && (!input.hidden || !input.hidden(dependentSelection[input.watch])) &&
-              <div key={input.name} witherror={errors[input.name] ? 'true' : 'false'} className='frm-inputField flex col marv10'>
-                <FormControl variant="outlined" key={input.name}>
-                  <InputLabel htmlFor="component-outlined">
-                    <span>{input.label} </span>
-                  </InputLabel>
-                  {
-                    input.type==='select' 
-                    ? <Controller 
-                    control={control}
-                    name={input.name}
-                    defaultValue={input.defaultValue}
-                    rules={{...input.validation, ...additionalValidation}}
-                    render={({ onChange, onBlur, value, name }) => (<Select
-                        label={input.label}
-                        error={errors[input.name] ? true : false} 
-                        onChange={(e)=>{onChange(e); resetWatchingInputs(e);}}
-                        onBlur={onBlur}
-                        value={value}
-                        name={name}
-                        disabled={disabled}
-                        >
-                        {input.options(dependentSelection[input.watch]).map(option=>(
-                          <MenuItem key={option.value} value={option.value}>{option.name}</MenuItem>
-                        ))}
-                        
-                    </Select>)}/>
-                    : <Controller 
-                    control={control}
-                    name={input.name}
-                    defaultValue={input.defaultValue}
-                    rules={{...input.validation, ...additionalValidation}}
-                    render={({ onChange, onBlur, value, name }) => (<OutlinedInput 
-                      label={input.label}                   
-                      onChange={onChange}
-                      onBlur={onBlur}
-                      value={value}
-                      name={name}
-                      disabled={disabled}
-                      error={errors[input.name] ? true : false}
-                      type={input.type || 'text'}
-                      {...adornments}
-                    />)}/>
-                  }
-                  
-                </FormControl>
-                <div className='frm-error fs12 marl3 mart5 cred light' >{errors[input.name] && errors[input.name].message}</div>
-              </div>
-              }
+                {(input.type !== 'select' || input.options(dependentSelection[input.watch]))
+                  && (!input.hidden || !input.hidden(dependentSelection[input.watch])) &&
+                  <div key={input.name} witherror={errors[input.name] ? 'true' : 'false'} className='frm-inputField flex col marv10'>
+                    <FormControl variant="outlined" key={input.name}>
+                      {type!=='switch' &&<InputLabel htmlFor="component-outlined">
+                        <span>{input.label} </span>
+                      </InputLabel>}
+                      <Controller
+                        control={control}
+                        name={input.name}
+                        defaultValue={input.defaultValue}
+                        rules={{ ...input.validation, ...additionalValidation }}
+                        render={({ onChange, onBlur, value, name }) => (
+                          <>
+                            {type === 'select' && 
+                              <Select
+                                label={input.label}
+                                error={errors[input.name] ? true : false}
+                                onChange={(e) => { onChange(e); resetWatchingInputs(e); }}
+                                onBlur={onBlur}
+                                value={value}
+                                name={name}
+                                disabled={disabled}>
+                                {input.options(dependentSelection[input.watch]).map(option => (
+                                  <MenuItem key={option.value} value={option.value}>{option.name}</MenuItem>
+                                  ))
+                                }
+                              </Select>
+                            }
+                            {type==='switch' &&
+                            <FormControlLabel
+                              control={
+                                <Switch
+                                  checked={value}
+                                  onChange={e => onChange(e.target.checked)}
+                                  color="primary"
+                                  name={name}
+                                  inputProps={{ 'aria-label': 'primary checkbox' }}
+                                />}
+                              label={input.label}/>
+                            }
+                            {type==='default' &&
+                              <OutlinedInput
+                                label={input.label}
+                                onChange={onChange}
+                                onBlur={onBlur}
+                                value={value}
+                                name={name}
+                                disabled={disabled}
+                                error={errors[input.name] ? true : false}
+                                type={input.type || 'text'}
+                                {...adornments} />
+                            }
+                          </>
+                            
+                        )} />
+
+                    </FormControl>
+                    <div className='frm-error fs12 marl3 mart5 cred light' >{errors[input.name] && errors[input.name].message}</div>
+                  </div>
+                }
               </>
             )
           }
-          
+
         })
       }
-      </div>
+    </div>
   )
-  const submitButton=(onClick)=>(
+  const submitButton = (onClick) => (
     <Button variant="contained"
-        classes={{
-          root: 'frm-submitButton'
-        }} 
-        type='submit' 
-        disabled={pending || disabled}
-        onClick={onClick}>
-        {!pending ? submitText : "En cours..."} 
+      classes={{
+        root: 'frm-submitButton'
+      }}
+      type='submit'
+      disabled={pending || disabled}
+      onClick={onClick}>
+      {!pending ? submitText : "En cours..."}
     </Button>
   )
-  const submitAction=handleSubmit(cleanAndSubmit)
-  return(
-  <FormLayout 
-    title={title}
-    cancelDisabled={pending}
-    content={formContent()} 
-    isDialog={isDialog} 
-    submitAction={submitAction}
-    submitButton={submitButton}
-    cancel={cancel}/>
+  const submitAction = handleSubmit(cleanAndSubmit)
+  return (
+    <FormLayout
+      title={title}
+      cancelDisabled={pending}
+      content={formContent()}
+      isDialog={isDialog}
+      submitAction={submitAction}
+      submitButton={submitButton}
+      cancel={cancel} />
   )
 }
 
