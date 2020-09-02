6affd609b730dd84dade0991f5ab339778180da1
Besma <db_rabia_cherif@edu.esi.dz>
2020-09-02 08:41:28 +0100

    map added

---
+++ src/components/misc/Map/Map.js
@@ -0,0 +1,32 @@
+
+import React, { Component, useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+import { apiConstants } from 'consts/api.constants';
+import './Map.scss';
+
+mapboxgl.accessToken = apiConstants.MAPBOXGL_TOKEN
+
+const Map = props => {
+  const {zoom, lng, lat}=props
+  const mapContainer = useRef()
+  
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+    const positionMarker = new mapboxgl.Marker({color:'var(--green)'})
+    positionMarker.setLngLat([lng,lat]).addTo(map);
+  }, [])
+
+  return (
+    <div className={`Map ${props.className || ''}`}>
+      <div ref={mapContainer} className='map-container relh100' />
+    </div>
+  )
+}
+
+export default Map
--- src/consts/api.constants.js
+++ src/consts/api.constants.js
@@ -1,3 +1,4 @@
 export const apiConstants={
-  URL : 'https://us-central1-faci-hospitality-master-3d3e7.cloudfunctions.net'
+  URL : 'https://us-central1-faci-hospitality-master-3d3e7.cloudfunctions.net',
+  MAPBOXGL_TOKEN : 'pk.eyJ1IjoiYmVzbWEiLCJhIjoiY2tlbDAzeTZoMGh3NzJ4bnczbG5oNHd6YyJ9.PGle7synZrxhcSEzWLUW8Q'
 }
